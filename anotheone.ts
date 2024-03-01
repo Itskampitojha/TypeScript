e6ce657f329b73dfdd26cb92dc57b5231d8ee239
kampit ojha <97059622+Itskampitojha@users.noreply.github.com>
2024-03-01 10:18:13 +0530

    hello ts

---
+++ anotheone.ts
@@ -0,0 +1,22 @@
+// hello.ts
+
+// Function to greet someone
+
+
+  
+  function greet(name: number): string{
+    return `hello ${name}!`;
+  }
+
+
+  const personName: number = 9;
+  const greeting: string = greet(personName);
+
+  console.log(greeting);
+  
+  
+
+  
+  
+  
+  
---
+++ hello.ts
@@ -0,0 +1,11 @@
+// hello.ts
+
+// Function to greet the world
+
+  
+  function niceboy() {
+    return 'Hello world';
+  }
+
+  console.log(niceboy());
+  
